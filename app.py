c9acdbd75b2840c406292e28fe81776b0631f51b
unknown <marton.szel@gmail.com>
2019-06-19 23:24:39 +0200

    trying flask

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def vacationmanagement(name=None):
+    return render_template(
+        "Vacationmanagement.html",
+        name=name,
+    )
+@app.route("/api/data/")
+def get_data():
+    return app.send_static_file("data.json")
